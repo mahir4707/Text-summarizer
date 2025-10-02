f1a035c6a426f91a24a5c92294f8d7e94e22a2fa
mahir4707 <mahirsama68@gmail.com>
2025-10-02 17:05:40 +0530

    updated app.py

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 # Example text
 text = """
-LangChain is an open-source framework designed to simplify the creation of applications using large language models (LLMs). It provides a standard interface for integrating with other tools and end-to-end chains for common applications. It helps AI developers connect LLMs such as GPT-4 with external data and computation. This framework comes for both Python and JavaScript.
-
-Key benefits include:
-
-Modular Workflow: Simplifies chaining LLMs together for reusable and efficient workflows.
-Prompt Management: Offers tools for effective prompt engineering and memory handling.
-Ease of Integration: Streamlines the process of building LLM-powered applications.
+Leader of opposition in the Lok Sabha, Rahul Gandhi, accused the BJP-led central government of mounting a “wholesale attack on Indian democracy” during an interaction at EIA University in Colombia. The BJP hit back, calling him a “Leader of Propaganda” and criticising him for "once again" denigrating Indian democracy on a foreign platform.
+"The single biggest risk is the attack on democracy that is taking place in India because India has multiple religions, multiple traditions, multiple languages. And India actually is a conversation between all its people and different traditions, different religions, different ideas require space. And that the best method for creating that space is the democratic system. And currently there is a wholesale attack on the democratic system in India," Rahul said during a recent interaction with students.
 """
 
 # Summarize
-summary = summarizer(text, max_length=50, min_length=25, do_sample=False)
+summary = summarizer(text, max_length=150, min_length=25, do_sample=False)
 
 print("Summary:", summary[0]['summary_text'])
